855d21c642ad3c95a9bd491025434fdd33a6f896
Fabian Deifuss <deifussfabian@gmail.com>
2020-06-25 15:17:45 +0200

    moved logic into main. no semantic changes

--- src/client/main.py
+++ src/client/main.py
@@ -13,44 +13,45 @@ except KeyError as key:
     print(f"Database connection cannot be established. {key} is unset! Please export it")
     exit(1)
 
-# construct a (fictitious) address book using a database for its phone numbers
-dbcon = pg.connect(
-    host     = CONTACTS_DB_HOST,
-    port     = CONTACTS_DB_PORT,
-    dbname   = CONTACTS_DB_NAME,
-    user     = CONTACTS_DB_USERNAME,
-    password = CONTACTS_DB_PASSWORD)
-cursor = dbcon.cursor()
-cursor.execute("""
-    SELECT number, last_interaction_utc 
-    FROM contacts;
-    """)
-address_book = [contact for contact in cursor.fetchall()]
-dbcon.close()
-cursor.close()
-
-# construct 2 dictionaries of number combinations:
-# NOTE THAT LAST_INTERACTION IS JUST AN EXAMPLE OF ANY PRIVATELY DISCLOSED INFORMATION
-# 1 - MY_NUMBER followed by A_FRIENDS_NUMBER : "last_interaction"
-# 2 - A_FRIENDS_NUMBER followed by MY_NUMBER : "last_interaction"
-# this is done so that we don't get any false positivies when receiving the intersection
-# -> upload the first dict (my perspective)
-# -> check for intersections using the second dict (each friends perspective)
-my_combinations_dict      = {}
-friends_combinations_dict = {}
-for contact in address_book:
-    contact_number           = str(contact[0])
-    contact_last_interaction = str(contact[1])
-    my_combination           = MY_PHONE_NUMBER + contact_number  + ':' + contact_last_interaction
-    friend_combination       = contact_number  + MY_PHONE_NUMBER + ':' + contact_last_interaction
-    my_combinations_dict     [hashlib.sha1(my_combination    .encode()).hexdigest()] = my_combination
-    friends_combinations_dict[hashlib.sha1(friend_combination.encode()).hexdigest()] = friend_combination
-my_combinations_array      = [hash_val for hash_val in my_combinations_dict]
-friends_combinations_array = [hash_val for hash_val in friends_combinations_dict]
-
 
 
 if __name__ == "__main__":
+    # construct a (fictitious) address book using a database for its phone numbers
+    dbcon = pg.connect(
+        host     = CONTACTS_DB_HOST,
+        port     = CONTACTS_DB_PORT,
+        dbname   = CONTACTS_DB_NAME,
+        user     = CONTACTS_DB_USERNAME,
+        password = CONTACTS_DB_PASSWORD)
+    cursor = dbcon.cursor()
+    cursor.execute("""
+        SELECT number, last_interaction_utc 
+        FROM contacts;
+        """)
+    address_book = [contact for contact in cursor.fetchall()]
+    dbcon.close()
+    cursor.close()
+
+    # construct 2 dictionaries of number combinations:
+    # NOTE THAT LAST_INTERACTION IS JUST AN EXAMPLE OF ANY PRIVATELY DISCLOSED INFORMATION
+    # 1 - MY_NUMBER followed by A_FRIENDS_NUMBER : "last_interaction"
+    # 2 - A_FRIENDS_NUMBER followed by MY_NUMBER : "last_interaction"
+    # this is done so that we don't get any false positivies when receiving the intersection
+    # -> upload the first dict (my perspective)
+    # -> check for intersections using the second dict (each friends perspective)
+    my_combinations_dict      = {}
+    friends_combinations_dict = {}
+    for contact in address_book:
+        contact_number           = str(contact[0])
+        contact_last_interaction = str(contact[1])
+        my_combination           = MY_PHONE_NUMBER + contact_number  + ':' + contact_last_interaction
+        friend_combination       = contact_number  + MY_PHONE_NUMBER + ':' + contact_last_interaction
+        my_combinations_dict     [hashlib.sha1(my_combination    .encode()).hexdigest()] = my_combination
+        friends_combinations_dict[hashlib.sha1(friend_combination.encode()).hexdigest()] = friend_combination
+    my_combinations_array      = [hash_val for hash_val in my_combinations_dict]
+    friends_combinations_array = [hash_val for hash_val in friends_combinations_dict]
+
+
     # check for intersections
     # returns an array of already registered hashes
     resp = requests.get(f"http://{BASE_URL}/compare/", data=json.dumps(friends_combinations_array)).json()
